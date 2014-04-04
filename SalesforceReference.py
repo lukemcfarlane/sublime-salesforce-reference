b04eb4d034d5e8d4de526e120147b3539287f73d
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-04-05 11:16:44 +1300

    New index class stores index data and gets passed RetrieveIndexThread class.

--- SalesforceReference.py
+++ SalesforceReference.py
@@ -29,17 +29,22 @@ def plugin_loaded():
 class SalesforceReferenceCommand(sublime_plugin.WindowCommand):
 
     def run(self):
-        thread = RetrieveIndexThread(self.window)
+        index = Index(
+            "http://www.salesforce.com/us/developer/docs/apexcode/Data/Toc.xml", 
+            "http://www.salesforce.com/us/developer/docs/apexcode/",
+            "Getting Started"
+        )
+        thread = RetrieveIndexThread(self.window, index, True)
         thread.start()
         ThreadProgress(thread, 'Retrieving Salesforce Reference Index...', '')
 
 
 class RetrieveIndexThread(threading.Thread):
     """
-    A thread to run retrieval of the Saleforce Documentation index, or access the reference_cache
+    A thread to run retrieval of a given Salesforce Documentation index, or access the reference_cache
     """
 
-    def __init__(self, window, open_when_done=True):
+    def __init__(self, window, index, open_when_done=True):
         """
         :param window:
             An instance of :class:`sublime.Window` that represents the Sublime
@@ -48,18 +53,22 @@ class RetrieveIndexThread(threading.Thread):
             Whether this thread is being run solely for caching, or should open
             the documentation list for user selection when done. Defaults to
             true - should open the documentaton list when done
+        :param index
+            An index instance containing the endpoint (toc.xml) and section title
+            e.g. index = Index("http://www.salesforce.com/us/developer/docs/apexcode/Data/Toc.xml", "Reference")
         """
-
         self.window = window
         self.open_when_done = open_when_done
+        self.index = index
         global reference_cache
         threading.Thread.__init__(self)
 
     def run(self):
         if not (reference_cache.sf_ref_pages_titles and reference_cache.sf_ref_pages_links):
-            sf_xml = urllib.request.urlopen('http://www.salesforce.com/us/developer/docs/apexcode/Data/Toc.xml').read().decode('utf-8')
+            sf_xml = urllib.request.urlopen(self.index.endpoint).read().decode('utf-8')
             sf_tree = ElementTree.fromstring(sf_xml)
-            leaf_parents = sf_tree.findall("*[@Title='Reference'].//TocEntry[@DescendantCount='0']..")
+
+            leaf_parents = sf_tree.findall("*[@Title='" + self.index.section + "'].//TocEntry[@DescendantCount='0']..")
             for parent in leaf_parents:
                 reference_cache.sf_ref_pages_titles.append(parent.attrib['Title'])
                 reference_cache.sf_ref_pages_links.append(parent.attrib['Link'])
@@ -69,5 +78,12 @@ class RetrieveIndexThread(threading.Thread):
 
     def open_documentation(self, reference_index):
         if(reference_index != -1):
-            base_url= 'http://www.salesforce.com/us/developer/docs/apexcode'
+            base_url= self.index.base_url
             webbrowser.open_new_tab(base_url + reference_cache.sf_ref_pages_links[reference_index])
+
+
+class Index():
+    def __init__(self, endpoint, base_url, section):
+        self.base_url = base_url
+        self.endpoint = endpoint
+        self.section = section
